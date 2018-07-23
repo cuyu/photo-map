9c1e288630a172db0485e5a2d30d8f2a1f04d81d
cuyu <icyarm@qq.com>
2018-07-23 16:26:07 +0800

    Add simple example of leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import {LatLngTuple} from "leaflet";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 13,
+    };
+
+    public render() {
+        const position: LatLngTuple = [this.state.lat, this.state.lng];
+        return (
+            <Map center={position} zoom={this.state.zoom} className='Map'>
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br/> Easily customizable.
+                    </Popup>
+                </Marker>
+            </Map>
+        );
+    }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'leaflet/dist/leaflet.css'  // Must import the leaflet css to make map tiles works!
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
